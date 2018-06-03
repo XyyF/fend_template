422dfdafdfe0e60a01dd199235405cf8a980b310
xiaoyufei <915724526@qq.com>
2018-06-03 22:41:51 +0800

    feat: test slidefullpage plugin

---
+++ src/slideFullPage.js
@@ -0,0 +1,5 @@
+import SlideFullPage from 'slidefullpage';
+
+window.SlideFullPage = SlideFullPage;
+
+window.b = 1231;
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -19,7 +19,7 @@ module.exports = {
         ],
     },
     devServer: {
-        port: 3000,
+        port: 3100,
         contentBase: './',
     },
 }
